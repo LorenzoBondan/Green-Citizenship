44531d184b29d0610ae2351f7b88b56b04183e2d
LorenzoBondan <lbbondan@gmail.com>
2025-03-21 15:12:31 -0300

    backend created

---
+++ backend/src/main/java/br/ucs/greencitizenship/GreencitizenshipApplication.java
@@ -0,0 +1,13 @@
+package br.ucs.greencitizenship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreencitizenshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreencitizenshipApplication.class, args);
+	}
+
+}
